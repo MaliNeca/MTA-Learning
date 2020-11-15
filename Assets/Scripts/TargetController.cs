89a4ca1bf79f94514475c9c303c93f9fe0f1f5c8
MaliNeca <borcanac.c1@gmail.com>
2020-11-15 16:56:19 +0100

    v1.2

--- Assets/Scripts/TargetController.cs
+++ Assets/Scripts/TargetController.cs
@@ -2,13 +2,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Vuforia;
 
 public class TargetController : MonoBehaviour
 {
+    private SpriteRenderer spriteRenderer;
+
+    public Transform imageTarget;
+    private Vector3 myVector;
+    public Transform ARCamera;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        /*myVector = new Vector3(ARCamera.position.x + myVector.x, myVector.y, ARCamera.position.z + myVector.z);
+        StartCoroutine(positionSync());*/
     }
 
 
@@ -16,20 +28,48 @@ public class TargetController : MonoBehaviour
     void Update()
     {
         
+        transform.position = new Vector3(imageTarget.position.x, transform.position.y, imageTarget.position.z);
+        myVector = transform.position;
     }
 
 
     public void setObjectActive(bool _isActive)
     {
 
-        GetComponent<SpriteRenderer>().enabled = _isActive;
+        //GetComponent<SpriteRenderer>().enabled = _isActive;
+        
+
+
+        //update the position
+        //transform.position = currentPos + vectorImageTarget;
+        
+        spriteRenderer.enabled = _isActive;
         GetComponent<PhotonView>().RPC("setActiveSync", RpcTarget.AllBuffered, _isActive);
     }
 
     [PunRPC]
     public void setActiveSync(bool _isActive)
     {
+
+        //transform.position = new Vector3(ARCamera.position.x + myVector.x, myVector.y,  ARCamera.position.z + myVector.z);
         GetComponent<SpriteRenderer>().enabled = _isActive;
     }
 
+    [PunRPC]
+    public void setPositionSync(Vector3 newPosition)
+    {
+        
+        transform.position = newPosition;
+    }
+
+    public IEnumerator positionSync()
+    {
+        while (true)
+        {
+            yield return new WaitForSecondsRealtime(0.2f);
+            GetComponent<PhotonView>().RPC("setPositionSync", RpcTarget.AllBuffered, myVector);
+
+        }
+    }
+   
 }
---
+++ Assets/SharedParam.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharedParam : MonoBehaviour
+{
+    private int broj;
+
+   public int GetBroj()
+    {
+        return broj;
+
+    }
+
+    public void SetBroj(int b)
+    {
+        this.broj = b;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
